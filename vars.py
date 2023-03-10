1f154a176223df7a56c3c97173a5c58b68a26d41
Tyyntm <126965835+Tyyntm@users.noreply.github.com>
2023-03-10 21:07:17 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-api_id = ''
-api_hash = ''
-bot_token = ''
-session = ''
-time = ''
-chats = ''
-white_list = ''
+api_id = "9735241"
+api_hash = "24abe5c480e78c1ccbe1ece0afAAHqHS"
+bot_token = '6016020225:AAHqHSAnrRukFKp6hjof3WpNEsF8YBIp15g'
+session = 'BQCUjEkAPQtEjCd3V9qBAKFXzd0M6Qoq9TVhJ5RCmg4CwhO8XUJ8zjK5hDbfsmmie04aHUh5Zm2P31E2ZGsMn0MilzLcMoVlF-asyKLZZav39gJhXVPwQqhk-fzgKVdtHirYi6E4DzP0mcM1tb-_t1Fxg5oJVYjjXRLFtsbIQb3yXgYV83TkC_9fvWgFAAUCYI2L3BCKfAMZBaatIYnUTk_SiABhRRnL557Yq3aYu4WCegP2F7dw7vGSkP4rB-Yqya7ufTJbYhOYfGjJZEx9IY5pi1zWKLHWBodvPfZwwPsTmoruicJD2p4iJF4ttPqjm6DZ48DvYdVrsToJEW368sXhmjMZDQAAAAFNR5C3AA'
+time = '1800'
+chats = '-1001816566628 -1001858656893'
+white_list = '-1001816566628 -1001858656893'
 black_list = ''
-database_uri = ''
+database_uri = 'mongodb+srv://HL:HL@cluster0.g8so1zk.mongodb.net/?retryWrites=true&w=majority'
